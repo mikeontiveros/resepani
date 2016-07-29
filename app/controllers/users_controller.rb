26fc9864b441466a64e3419e00eef20ef3621ec9
Michael Ontiveros <onerun@gmail.com>
2016-07-29 04:39:09 -0700

    Finished user update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,12 @@ class UsersController < ApplicationController
 #        @user = User.find(params[:id])
     end
 
+    def update
+        if current_user.update_attributes(params.require(:user).permit(:name, :latitude, :longitude, :role))
+            redirect_to user_path(current_user)
+        else
+            render :edit
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     get 'welcome/index'
     get "users/:id" => "users#show", as: :user
     get "users/:id/edit" => "users#edit", as: :edit_user
+    patch "users/:id" => "users#update"
 
     # The priority is based upon order of creation: first created -> highest priority.
     # See how all your routes lay out with "rake routes".
