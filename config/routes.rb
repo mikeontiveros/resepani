e8d58c479b050a8ceb723d2e635b64373d6ecab4
Michael Ontiveros <onerun@gmail.com>
2016-07-29 03:37:16 -0700

    Style adjustments, prioritize devise route, add role to users#show and add role dropdown box to users#edit.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
+    devise_for :users
+
     get 'welcome/index'
     get "users/:id" => "users#show", as: :user
     get "users/:id/edit" => "users#edit", as: :edit_user
 
-    devise_for :users
     # The priority is based upon order of creation: first created -> highest priority.
     # See how all your routes lay out with "rake routes".
 
