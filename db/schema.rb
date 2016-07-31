661b777d2343202e63aa2179f4ee3849ada155da
Michael Ontiveros <onerun@gmail.com>
2016-07-31 15:29:06 -0700

    Create and seed Service model. Devise structure and styling. Minor changes to application styling.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729173422) do
+ActiveRecord::Schema.define(version: 20160729222329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20160729173422) do
     t.integer  "gender"
     t.date     "dob"
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string   "name"
+    t.text     "desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 services = Service.create([
-    {type: 'Other', desc: "The less said, the better."},
-    {type: 'Nanny', desc: "I watch your kids so you don't have to."},
-    {type: 'Tutor', desc: "I teach your kids so you don't have to."}
+    {name: "Other", desc: "A catch-all cetegory"},
+    {name: "Nanny", desc: "I watch your kids so you don't have to."},
+    {name: "Tutor", desc: "I teach your kids so you don't have to."}
   ])
