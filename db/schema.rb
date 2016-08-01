a6574697ba333633c9b416c7ef7f7bb9f7338118
Michael Ontiveros <onerun@gmail.com>
2016-07-31 19:53:38 -0700

    Remove name from and add first name, last name, username and address to users. Modify forms to reflect changed User model columns. Update User edit/show style.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729222329) do
+ActiveRecord::Schema.define(version: 20160731233702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,9 +46,12 @@ ActiveRecord::Schema.define(version: 20160729222329) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "address"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
